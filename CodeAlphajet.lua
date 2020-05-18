407e173ac6e57130bda5f65bdbe64f8589e0b8e1
Airone44 <e.renaudin@hotmail.com>
2020-05-18 23:36:31 +0200

    Ajout fichier gadget

---
+++ CodeAlphajet.lua
@@ -0,0 +1,18 @@
+crew_size	 = 2,
+	crew_members = 
+	{
+		[1] = 
+		{
+			ejection_seat_name = "pilot+ejectionseat",
+			drop_canopy_name = "Front_Canopy",
+			pos =  {2.831,	-0.60,	0},     --  {2.831,	-0.7,	0}, altitude =2eme parametre
+			canopy_pos = {0,	0,	0},
+		}, -- end of [1]
+		[2] = 
+		{
+			ejection_seat_name = "pilot+ejectionseat",
+			drop_canopy_name = "Rear_Canopy",
+			pos =  {1.231,	0,	0},     --  {2.43,	-0.6,	0}, altitude =2eme parametre
+			canopy_pos = {0,	0,	0},
+		}, -- end of [2]
+	}, -- end of crew_members
